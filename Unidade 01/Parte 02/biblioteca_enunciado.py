2cb85e4644cc7a8c48ac5b99d496adcd4c86639d
sthevanbello <sthevan.alves@gmail.com>
2022-04-22 13:37:52 -0300

    Function for return all items

--- Unidade 01/Parte 02/biblioteca_enunciado.py
+++ Unidade 01/Parte 02/biblioteca_enunciado.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
 #quero usar o banco de dados nesse arquivo, usando o formato sqlite
-engine = create_engine('sqlite:///biblioteca.db')
+engine = create_engine('sqlite:///D:/Developer/Impacta/Semestre3/Desenvolvimento de APIs e Microsserviços/Python/Unidade 01/Parte 02/biblioteca.db')
 
 #mas se quisesse uma solução mais robusta, poderia
 #usar mudando o código muito pouco
@@ -11,14 +11,14 @@ engine = create_engine('sqlite:///biblioteca.db')
 
 #criar a tabela
 def criar_tabelas():
-    with engine.connect() as con:    
+    with engine.connect() as con:
         create_tabela_aluno = """
         CREATE TABLE IF NOT EXISTS Aluno (
             id INTEGER PRIMARY KEY,
             nome TEXT NOT NULL,
             email TEXT NOT NULL UNIQUE
         )
-        """    
+        """
         rs = con.execute(create_tabela_aluno)
         create_tabela_livro = """
         CREATE TABLE IF NOT EXISTS Livro (
@@ -27,17 +27,19 @@ def criar_tabelas():
             descricao TEXT NOT NULL,
             FOREIGN KEY(id_aluno) REFERENCES Aluno(id)  
         )
-        """   
+        """
         rs = con.execute(create_tabela_livro)
 
+
 def criar_alunos():
-    with engine.connect() as con:     
+    with engine.connect() as con:
         add_aluno = "INSERT INTO Aluno (id,nome,email) VALUES (1,'Lucas Mendes', 'lucas.mendes@exemplo.com');"
         rs = con.execute(add_aluno)
         add_aluno = "INSERT INTO Aluno (id,nome,email) VALUES (2,'Helena O. S.', 'helena@exemplo.com');"
         rs = con.execute(add_aluno)
         add_aluno = "INSERT INTO Aluno (id,nome,email) VALUES (3,'Mirtes', 'teescrevoumemail@exemplo.com');"
         rs = con.execute(add_aluno)
+
 
 def criar_livros():
     with engine.connect() as con:
@@ -48,11 +50,15 @@ def criar_livros():
         add_livro = "INSERT INTO Livro (id_livro, id_aluno, descricao) VALUES (3,2,'Gravidade')"
         rs = con.execute(add_livro)
 
+
 #criar_tabelas()
 #criar_alunos()
 
+
 class AlunoNaoExisteException(Exception):
     pass
+
+
 #estamos definindo uma classe AlunoNaoExisteException, que
 #herda todas as funcionalidades, todos os métodos e atributos,
 #de Exception
@@ -60,29 +66,27 @@ class AlunoNaoExisteException(Exception):
 #as coisas que AlunoNaoExisteException faz diferente de Exception
 #mas não fizemos mudança nenhuma (por isso pass)
 
-
-
-
 # 1) Crie uma função consulta_aluno que recebe a id de um aluno e devolve
 # um dicionário com os dados desse aluno
 
 #idéia da query no sql
-"SELECT * FROM aluno WHERE id = 2"
+# "SELECT * FROM aluno WHERE id = 2"
+
 
 def consulta_aluno(id_aluno):
-    with engine.connect() as con:   #conectar ao banco de dados 
-        sql_consulta = text ("SELECT * FROM aluno WHERE id = :id_do_aluno")
+    with engine.connect() as con:  #conectar ao banco de dados
+        sql_consulta = text("SELECT * FROM aluno WHERE id = :id_do_aluno")
         # id          : nome da coluna da tabela
         # id_aluno    : nome da variavel python que a funcao recebeu
         # id_do_aluno : nome do "buraco" definido na query sql_consulta
         #               nome passado como argumento do con.execute
-        rs = con.execute(sql_consulta, id_do_aluno = id_aluno)
+        rs = con.execute(sql_consulta, id_do_aluno=id_aluno)
         #executamos a query definida em sql_consulta, preenchendo o "buraco" :id_do_aluno
         result = rs.fetchone()
         # fetchone: pega uma linha do resultado
         # se tiver mais linhas, podemos pegar a próxima com outro fetchone
         # se não tiver mais linhas, receberemos um None
-        if result == None: #se a query retornou 0 linhas, o primeiro fetchone já resulta None
+        if result == None:  #se a query retornou 0 linhas, o primeiro fetchone já resulta None
             raise AlunoNaoExisteException
         # As linhas que o sqlalchemy devolve são parecidas com dicionários,
         # mas não é exatamente a mesma coisa. Por isso, fazemos uma conversão
@@ -92,15 +96,48 @@ def consulta_aluno(id_aluno):
 #1b) Crie uma função todos_alunos que retorna um lista com um dicionario
 # para cada aluno
 
+def todos_alunos():
+    list_dictionary = list()
+    dic = dict()
+    
+    with engine.connect() as con:
+        sql_consulta = text("SELECT * FROM aluno")
+        result_all = con.execute(sql_consulta)
+        while True:
+            tupla = result_all.fetchone()
+            if tupla == None:
+                break
+            list_dictionary.append(dict(tupla))
+    return list_dictionary
+
+# print(todos_alunos())
+# print(consulta_aluno(1))
+
 #1c) Crie uma função todos_livros que retorna um lista com um dicionario
 # para cada livro
+
+def todos_os_itens(tipo):
+    list_dictionary = list()
+    with engine.connect() as con:
+        sql_consulta = text(f"SELECT * FROM {tipo}")
+        result_all = con.execute(sql_consulta)
+        while True:
+            tupla = result_all.fetchone()
+            if tupla == None:
+                break
+            list_dictionary.append(dict(tupla))
+    return list_dictionary
+
+print(todos_os_itens("livro"))
+print(todos_os_itens("aluno"))
 
 # 2) Crie uma função cria livro que recebe os dados de um livro (id e descrição)
 # e o adiciona no banco de dados
 
+
+
 # 3) Crie uma função empresta_livro, que recebe a id de um livro, a id de um aluno
 # e marca o livro como emprestado pelo aluno
-
 
 # 4) Crie uma função devolve_livro, que recebe a id de um livro, e marca o livro
 # como disponível
@@ -110,5 +147,3 @@ def consulta_aluno(id_aluno):
 
 # 6) Crie uma função livros_do_aluno, recebe o nome do aluno e devolve a lista de todos
 # os livros que estão com o aluno no momento
-
-
